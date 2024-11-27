d11afffc2b1f2d93125545a1cc0f568b96ad3e56
AmoInno <amogelangmashishi12@gmail.com>
2024-11-27 13:15:51 +0200

    install tailwindcss

--- podcast-app/src/App.jsx
+++ podcast-app/src/App.jsx
@@ -1,14 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div>
-      <div>Hello World</div>
-    </div>
-  );
+  return <h1 className="text-8xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
